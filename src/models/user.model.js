3691f7d79084fde932bdb7ee843e91612f3f142c
Mohammad Iqbal <iqbal240456@gmail.com>
2022-08-02 11:27:20 +0530

    added user schema

---
+++ src/models/user.model.js
@@ -0,0 +1,34 @@
+const mongoose= require("mongoose");
+const  bcrypt = require('bcryptjs');
+
+const userSchema= new mongoose.Schema({
+    username:{type:String,required:true,},
+    phone:{type:String,required:true},
+    email:{type:String,required:true,unique:true},
+    password:{type:String,required:true},
+    roles:[{type:String}]
+},{
+    versionKey:false,
+    timestamps:true
+})
+
+// hashing the password before saving in db
+
+userSchema.pre("save",function(next){
+    // if the password is already hashed then just return
+    if(!this.isModified("password")){
+        return next();
+    }
+    const hash= bcrypt.hashSync(this.password,8);
+    this.password=hash;
+    return next();
+    // else we do the hashing
+})
+
+// adding a method to check the password
+userSchema.methods.checkPassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+  
+
+module.exports= mongoose.model("user",userSchema);
