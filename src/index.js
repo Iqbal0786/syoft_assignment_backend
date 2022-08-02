2311c544ff1e9fda797e71f52e449d342420aa7a
Mohammad Iqbal <iqbal240456@gmail.com>
2022-08-02 12:52:39 +0530

    added product controller

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express= require("express");
 const connect= require("./configs/db")
 const {register,login}= require("./controllers/auth.controller")
 const userController= require("./controllers/user.controller")
+const productController= require("./controllers/product.controller")
 const cors= require("cors")
 const app= express();
 app.use(cors())
@@ -10,6 +11,7 @@ app.use(express.json());
 app.post("/register",register)
 app.post("/login",login)
 app.use("/users",userController)
+app.use("/products",productController)
 app.listen(7777,async()=>{
     try {
         await connect();
