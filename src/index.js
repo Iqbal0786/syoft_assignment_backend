f7328fbd7b6de19fd218dd248e18e642312fe0b7
Mohammad Iqbal <iqbal240456@gmail.com>
2022-08-02 11:52:40 +0530

    added user controller

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const express= require("express");
 const connect= require("./configs/db")
+const {register,login}= require("./controllers/auth.controller")
+const userController= require("./controllers/user.controller")
 const cors= require("cors")
 const app= express();
 app.use(cors())
 app.use(express.json());
 
-
-
+app.post("/register",register)
+app.post("/login",login)
+app.use("/users",userController)
 app.listen(7777,async()=>{
     try {
         await connect();
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -6,7 +6,7 @@ const userSchema= new mongoose.Schema({
     phone:{type:String,required:true},
     email:{type:String,required:true,unique:true},
     password:{type:String,required:true},
-    roles:[{type:String}]
+    roles:[{type:String , required:true}]
 },{
     versionKey:false,
     timestamps:true
